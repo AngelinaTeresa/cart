12c6ffece84fb14ca7d7ae85922932adfaa66502
Angelina Teresa <Angelina.Teresa@lntinfotech.com>
2022-07-15 18:40:23 +0530

    Changes for eureka client

--- src/main/java/com/shopping/cart/config/RestConfig.java
+++ src/main/java/com/shopping/cart/config/RestConfig.java
@@ -1,6 +1,7 @@
 package com.shopping.cart.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.time.Duration;
 public class RestConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
         return builder
